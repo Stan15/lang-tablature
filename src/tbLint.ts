e5ae03f46adfc73e4669a347f6e69b138411da04
Stanley Ihesiulo <stanihe1901@gmail.com>
2021-08-29 17:04:26 -0400

    implemented preliminary connection to linter

--- src/tbLint.ts
+++ src/tbLint.ts
@@ -1,9 +1,10 @@
-import { Diagnostic, linter } from "@codemirror/lint";
+import { Diagnostic } from "@codemirror/lint";
 import { Text } from "@codemirror/state";
 import { EditorView } from "@codemirror/view";
 import { tablatureLanguage } from "./tablature";
+import {TabLinter} from "lint-tablature"
 
-export function tbLint(tblint: any, config?: any) {
+export function tbLint(tblint: TabLinter, config?: any) {
   if (!config) {
     config = {
       parserOptions: { auto: true, guitar: false, drum: false, bass: false },
@@ -15,65 +16,67 @@ export function tbLint(tblint: any, config?: any) {
   }
 
   return (view: EditorView) => {
-    let { state } = view,
-      found: Diagnostic[] = [];
-    console.log(`completing...\nstate: ${state}`);
-    for (let { from, to } of tablatureLanguage.findRegions(state)) {
-      let fromLine = state.doc.lineAt(from),
-        offset = {
-          line: fromLine.number - 1,
-          col: from - fromLine.from,
-          pos: from,
-        };
-      for (let d of tblint.verify(state.sliceDoc(from, to), config))
-        found.push(translateDiagnostic(d, state.doc, offset));
-    }
-    return found;
+    return tblint.verify(view.state, config)
+    //   found: Diagnostic[] = [];
+    // for (let d of tblint.verify(state, config))
+    //     found.push(translateDiagnostic(d, state.doc))
+    // console.log(`completing...\nstate: ${state}`);
+    // for (let { from, to } of tablatureLanguage.findRegions(state)) {
+    //   let fromLine = state.doc.lineAt(from),
+    //     offset = {
+    //       line: fromLine.number - 1,
+    //       col: from - fromLine.from,
+    //       pos: from,
+    //     };
+    //   for (let d of tblint.verify(state.sliceDoc(from, to), config))
+    //     found.push(translateDiagnostic(d, state.doc, offset));
+    // }
+    // return found;
   };
 }
 
-function mapPos(
-  line: number,
-  col: number,
-  doc: Text,
-  offset: { line: number; col: number; pos: number }
-) {
-  return (
-    doc.line(line + offset.line).from + col + (line == 1 ? offset.col - 1 : -1)
-  );
-}
+// function mapPos(
+//   line: number,
+//   col: number,
+//   doc: Text,
+//   offset: { line: number; col: number; pos: number }
+// ) {
+//   return (
+//     doc.line(line + offset.line).from + col + (line == 1 ? offset.col - 1 : -1)
+//   );
+// }
 
-function translateDiagnostic(
-  input: any,
-  doc: Text,
-  offset: { line: number; col: number; pos: number }
-): Diagnostic {
-  let start = mapPos(input.line, input.column, doc, offset);
-  let result: Diagnostic = {
-    from: start,
-    to:
-      input.endLine != null && input.endColumn != 1
-        ? mapPos(input.endLine, input.endColumn, doc, offset)
-        : start,
-    message: input.message,
-    source: input.ruleId ? "jshint:" + input.ruleId : "jshint",
-    severity: input.severity == 1 ? "warning" : "error",
-  };
-  if (input.fix) {
-    let { range, text } = input.fix,
-      from = range[0] + offset.pos - start,
-      to = range[1] + offset.pos - start;
-    result.actions = [
-      {
-        name: "fix",
-        apply(view: EditorView, start: number) {
-          view.dispatch({
-            changes: { from: start + from, to: start + to, insert: text },
-            scrollIntoView: true,
-          });
-        },
-      },
-    ];
-  }
-  return result;
-}
+// function translateDiagnostic(
+//   input: any,
+//   doc: Text,
+//   offset: { line: number; col: number; pos: number }
+// ): Diagnostic {
+//   let start = mapPos(input.line, input.column, doc, offset);
+//   let result: Diagnostic = {
+//     from: start,
+//     to:
+//       input.endLine != null && input.endColumn != 1
+//         ? mapPos(input.endLine, input.endColumn, doc, offset)
+//         : start,
+//     message: input.message,
+//     source: input.ruleId ? "jshint:" + input.ruleId : "jshint",
+//     severity: input.severity == 1 ? "warning" : "error",
+//   };
+//   if (input.fix) {
+//     let { range, text } = input.fix,
+//       from = range[0] + offset.pos - start,
+//       to = range[1] + offset.pos - start;
+//     result.actions = [
+//       {
+//         name: "fix",
+//         apply(view: EditorView, start: number) {
+//           view.dispatch({
+//             changes: { from: start + from, to: start + to, insert: text },
+//             scrollIntoView: true,
+//           });
+//         },
+//       },
+//     ];
+//   }
+//   return result;
+// }
