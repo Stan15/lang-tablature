ea840cfe8570dc4bd9aa3efa55a2e7c353ee4fff
Stanley Ihesiulo <stanihe1901@gmail.com>
2021-10-02 01:57:20 -0400

    connected linter to editor

--- src/tbLint.ts
+++ src/tbLint.ts
@@ -1,10 +1,9 @@
 import { Diagnostic } from "@codemirror/lint";
 import { Text } from "@codemirror/state";
 import { EditorView } from "@codemirror/view";
-import { tablatureLanguage } from "./tablature";
-import {TabLinter} from "lint-tablature"
+import TABLint from "tablint"
 
-export function tbLint(tblint: TabLinter, config?: any) {
+export function tbLint(config?: any) {
   if (!config) {
     config = {
       parserOptions: { auto: true, guitar: false, drum: false, bass: false },
@@ -16,67 +15,42 @@ export function tbLint(tblint: TabLinter, config?: any) {
   }
 
   return ((view: EditorView) => {
-    return tblint.verify(view.state, config)
-    //   found: Diagnostic[] = [];
-    // for (let d of tblint.verify(state, config))
-    //     found.push(translateDiagnostic(d, state.doc))
-    // console.log(`completing...\nstate: ${state}`);
-    // for (let { from, to } of tablatureLanguage.findRegions(state)) {
-    //   let fromLine = state.doc.lineAt(from),
-    //     offset = {
-    //       line: fromLine.number - 1,
-    //       col: from - fromLine.from,
-    //       pos: from,
-    //     };
-    //   for (let d of tblint.verify(state.sliceDoc(from, to), config))
-    //     found.push(translateDiagnostic(d, state.doc, offset));
-    // }
-    // return found;
+    // return tblint.verify(view.state, config)
+    let {state} = view, found: Diagnostic[] = [];
+    let lintResults = TABLint.lint(state.sliceDoc(0), config)
+    for (let d of lintResults)
+        found.push(translateDiagnostic(d, state.doc))
+    return found;
   }) as (view: EditorView) => any[];
 }
 
-// function mapPos(
-//   line: number,
-//   col: number,
-//   doc: Text,
-//   offset: { line: number; col: number; pos: number }
-// ) {
-//   return (
-//     doc.line(line + offset.line).from + col + (line == 1 ? offset.col - 1 : -1)
-//   );
-// }
-
-// function translateDiagnostic(
-//   input: any,
-//   doc: Text,
-//   offset: { line: number; col: number; pos: number }
-// ): Diagnostic {
-//   let start = mapPos(input.line, input.column, doc, offset);
-//   let result: Diagnostic = {
-//     from: start,
-//     to:
-//       input.endLine != null && input.endColumn != 1
-//         ? mapPos(input.endLine, input.endColumn, doc, offset)
-//         : start,
-//     message: input.message,
-//     source: input.ruleId ? "jshint:" + input.ruleId : "jshint",
-//     severity: input.severity == 1 ? "warning" : "error",
-//   };
-//   if (input.fix) {
-//     let { range, text } = input.fix,
-//       from = range[0] + offset.pos - start,
-//       to = range[1] + offset.pos - start;
-//     result.actions = [
-//       {
-//         name: "fix",
-//         apply(view: EditorView, start: number) {
-//           view.dispatch({
-//             changes: { from: start + from, to: start + to, insert: text },
-//             scrollIntoView: true,
-//           });
-//         },
-//       },
-//     ];
-//   }
-//   return result;
-// }
+function translateDiagnostic(
+  input: any,
+  doc: Text
+): Diagnostic {
+  let start = input.idx;
+  let result: Diagnostic = {
+    from: start,
+    to: input.endIdx ? input.endIdx : start,
+    message: input.message,
+    source: input.ruleId ? "tabhint:" + input.ruleId : "tabhint",
+    severity: input.severity == 1 ? "warning" : "error",
+  };
+  if (input.fix) {
+    let { range, text } = input.fix,
+      from = range[0],
+      to = range[1];
+    result.actions = [
+      {
+        name: "fix",
+        apply(view: EditorView, start: number) {
+          view.dispatch({
+            changes: { from: start + from, to: start + to, insert: text },
+            scrollIntoView: true,
+          });
+        },
+      },
+    ];
+  }
+  return result;
+}
